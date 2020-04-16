36a7447be47669f25a20048e6c52d193cf167ef5
Maura <maurawatkinson@gmail.com>
2020-04-16 17:22:40 +0100

    added some new files including the header php file

---
+++ Views/diarydist/entertainment.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Diary of a Distancer</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="diaryofad.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        
+   <?php
+   include 'header.php';
+   ?>
+  
+           
+  <div class="card-deck">
+      
+  <div class="card">
+    <img class="card-img-top" src="images/nflix.png"  height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">Netflix Top 10</h5>
+    <p class="card-text">My favourite crime dramas of the moment are as follows.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  </div>
+      
+  <div class="card">
+    <img class="card-img-top" src="images/boardgames.jpg" height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">The Ultimate Old-School Games</h5>
+    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  </div>
+      
+  <div class="card">
+    <img class="card-img-top" src="images/books.jpg"  height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">Books to take you somewhere</h6>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  </div>
+  </div>  
+        
+        
+        <br>
+        <br>
+        
+        
+        
+  <div class="card-deck">
+      
+  <div class="card">
+    <img class="card-img-top" src="images/secret .jpg"   height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">Secret Cinema at Home</h5>
+    <p class="card-text">New releases at Home</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+      
+  <div class="card">
+    <img class="card-img-top" src="images/spotify.jpg"  height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">Isolation Playlists</h5>
+    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+      
+  <div class="card">
+    <img class="card-img-top" src="images/games.jpg"  height="200" alt="Card image cap">
+    <div class="card-body">
+    <h6 class="card-title">Gaming</h6>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+   </div>
+  </div>
+      
+</div>  
+        
+        
+ 
+  
+        
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
+
--- Views/diarydist/homepage.php
+++ Views/diarydist/homepage.php
@@ -1,88 +1,73 @@
+
 <!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Diary of a Distancer</title>
+        <title>Diary of a Distancer.</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link href="diary.css" rel="stylesheet" type="text/css"/>
+        <link href="diaryofad.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap" rel="stylesheet">
     </head>
-    <body>
-       
-    <header>
-     
-        <h1>Diary of a Distancer. </h1>       
-            
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <!--<a class="navbar-brand" href="#">Home</a>-->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">About</a>
-      <a class="nav-item nav-link" href="#">Entertainment</a>
-       <a class="nav-item nav-link" href="#">Mind & Body</a>
-        <a class="nav-item nav-link" href="#">Food & Drink</a>
-      <a class="nav-item nav-link" href="#">Contact</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
-    </div>
-  </div>
-  <nav class="navbar navbar-light bg-light">
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</nav>
-</nav>    
+    
+    <body> 
         
-    </header>
+    <?php
+    include 'header.php';
+    ?>
+         
         
-        <br>
-        <br>
         
-        <div class="cont" align="center">
-       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
+        
+ 
+        
+        
+    <div class="cont" align="center">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        
     <div class="carousel-item active">
       <img src="images/tiger.jpg" height="350px" width="60%" alt="Tiger">
       <div class="carousel-caption d-none d-md-block">
-        <h5>The Wonderfully Weird Tiger King</h5>
-        <p class="car">Carol Baskin or Joe Exotic, who's side are you on?</p>
+      <h5>The Wonderfully Weird Tiger King</h5>
+      <p class="car">Carol Baskin or Joe Exotic, who's side are you on?</p>
       </div>
+      
     </div>
     <div class="carousel-item">
       <img src="images/trees.jpg" height="350px" width="60%"alt="Trees">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Books to lift your mood</h5>
-        <p class="car">My top picks so far during isolation.</p>
-      </div>
+      <h5>Books to lift your mood</h5>
+      <p class="car">My top picks so far during isolation.</p>
     </div>
+    </div>
+        
     <div class="carousel-item">
         <img src="images/bananas.jpg"height="350px" width="60%" alt="Bananas">
         <div class="carousel-caption d-none d-md-block">
         <h5>Banana Bread</h5>
         <p class="car">The new phenomenon,three ways.</p>
-      </div>
     </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    </div>
+    </div>
+        
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    </a>
+        
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
-</div>
-        </div>       
+    </a>
+    </div>
         
-        <br>
-        <br>
+    </div>       
+        
+    <br>
+    <br>
         
   <div class="card-deck">
   <div class="card">
@@ -93,6 +78,7 @@
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+      
   <div class="card">
     <img class="card-img-top" src="images/homejoe.jpg"  height="200" alt="Card image cap">
     <div class="card-body">
@@ -101,6 +87,7 @@
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+      
   <div class="card">
     <img class="card-img-top" src="images/workout.jpg"  height="200" alt="Card image cap">
     <div class="card-body">
@@ -109,6 +96,7 @@
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+      
 </div>     
         
         
