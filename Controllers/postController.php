7d68d21780ae993ab9dd730efc44a127fa114089
Zara <zarakletz@yahoo.co.uk>
2020-04-18 17:36:20 +0100

    post controller added

---
+++ Controllers/postController.php
@@ -0,0 +1,13 @@
+<?php
+
+$postID = $_POST['PostID'];
+$action = $_POST['action'];
+
+if ($action == 'update') {
+    //require to update post model
+}
+
+else {
+    //require to delete post model
+}
+
