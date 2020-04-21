aa7e230ead89f536ac9f08f6e3463c439057dc8e
Ciara Pimley <ciara_2465@hotmail.com>
2020-04-21 07:15:42 +0100

    updated controllers

--- Controllers/postController.php
+++ Controllers/postController.php
@@ -6,11 +6,19 @@ $action = $_POST['action'];
 if ($action == 'update') {
     session_start();
     $_SESSION["PostID"] = $postID;
-    header('Location: http://localhost/diary/Views/updatePost.php');
+    header('Location: http://localhost/BlogPost/Views/updatePost.php');
 }
 
 if ($action == 'updatePost') {
-    echo "post to be updated";
-} else {
+    require '../Model/update_post.php';
+    echo 'post updated';
+} 
+
+
+if ($action=='delete'){
+    require_once '../Model/delete_post.php';
+    
     //require to delete post model
 }
+
+?>
