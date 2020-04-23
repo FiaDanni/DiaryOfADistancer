d4d024bbde385e06ec169522b039bbe1c08f4d6c
Afua <afuadanquah@gmail.com>
2020-04-23 09:23:25 +0100

    fitness category updated

--- Views/fitness.php
+++ Views/fitness.php
@@ -1,52 +1,62 @@
 <?php
 include 'header.php';
-   ?>
+
+require_once '../Model/dbconfig.php';
+
+    try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //Prepare the statement
+    $statement = $pdo->prepare("SELECT posts.PostID, posts.Title, posts.Entry, posts.DateTime, categories.hashtag as categories
+    FROM posts
+    INNER JOIN categories
+    ON posts.PostID=categories.POSTID 
+    WHERE categories.hashtag='#Fitness' ORDER BY posts.DateTime DESC");
+    //Execute the Statement
+    $statement->execute();
+    //Put statement into variable to processed by the HTML page
+    
+    $categories = $statement->fetchall(PDO::FETCH_OBJ);
+    //var_dump($categories);
+    $pdo = null;
+    }
+catch(PDOException $e)
+    {
+    echo $statement . "<br>" . $e->getMessage(). "<br>";
+    }
+    
+    //var_dump($categories);
+    
+    $chunk = array_chunk($categories, 3);
+    //print_r($chunk);
+    
+    require_once 'fitness_1.php';
+    ?>
+
+<br>
+<div class="container-fluid">     
+ <div class="card-columns"  style="width:100%; padding-right: 10%; padding-left: 10%;">   
+     <?php foreach($categories as $category): ?>
+     
   
-        
-  <div class="card-deck" style="width:100%; padding-right: 10%; padding-left: 10%;" >
       
   <div class="card" > 
         
     <img class="card-img-top" src="images/homejoe.jpg" height="200"  alt="Card image cap">
     <div class="card-body">
-    <h6 class="card-title" a href="hompeage.php class="btn btn-primary stretched-link">PE with Joe</h6>
-    <p class="card-text">My favourite crime dramas of the moment are as follows.</p>
-  </div>
-    <div class="card-footer text-center"> 
-    <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
+    <h6 class="card-title" a href="hompeage.php class="btn btn-primary stretched-link"><?= $category->Title; ?></h6>
+    <!--<p class="card-text">My favourite crime dramas of the moment are as follows.</p>-->
+    <a href="#" class="btn btn-dark stretched-link">Tell me more</a>
     </div>
   </div>
-  
-      
-  <div class="card">
-    <img class="card-img-top" src="images/tidal.jpg" height="200" alt="Card image cap">
-    <div class="card-body">
-    <h6 class="card-title">Playlists to keep you going</h6>
-    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+     <br>
+      <?php endforeach; ?> 
   </div>
-    <div class="card-footer text-center"> 
-    <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
-    </div>
-  </div>
-      
-  <div class="card">
-    <img class="card-img-top" src="images/workout.jpg" height="200"  alt="Card image cap">
-    <div class="card-body">
-    <h6 class="card-title">Gentle Workouts</h6>
-    <p class="card-text">. This card has even longer content than the first to show that equal height action.</p>
-  </div>
-    <div class="card-footer text-center"> 
-    <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
-    </div>
-  </div>
-  </div>  
+</div>
         
         
-        <br>
-        <br>
-        
-        
-        
+<!--  <div class="container">        
   <div class="card-deck" style= "width:100%; padding-left: 10%; padding-right: 10%;">
       
       <div class="card">
@@ -54,8 +64,6 @@ include 'header.php';
     <div class="card-body">
     <h6 class="card-title">Yoga</h5>
     <p class="card-text">New releases at Home</p>
-    </div>
-    <div class="card-footer text-center"> 
     <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
     </div>
   </div>
@@ -65,8 +73,6 @@ include 'header.php';
     <div class="card-body">
     <h6 class="card-title">10mins cardio</h5>
     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer text-center"> 
     <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
     </div>
   </div>
@@ -76,13 +82,12 @@ include 'header.php';
     <div class="card-body">
     <h6 class="card-title">20 mins cardio</h6>
     <p class="card-text"> This card has even longer content than the first to show that equal height action.</p>
-   </div>
-    <div class="card-footer text-center"> 
     <a href="homepage.php" class="btn btn-dark stretched-link">Tell me more</a>
-    </div>
+   </div>
   </div>
       
 </div>  
+  </div>-->
 
         
         
